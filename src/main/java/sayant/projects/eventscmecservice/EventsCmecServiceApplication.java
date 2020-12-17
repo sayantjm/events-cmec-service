267ca492e62045fee82aea023efafa9c619bc615
Juanma Perales <sayantjm@gmail.com>
2020-12-17 20:30:06 +0100

    Creating the spring project. Closes #1

---
+++ src/main/java/sayant/projects/eventscmecservice/EventsCmecServiceApplication.java
@@ -0,0 +1,13 @@
+package sayant.projects.eventscmecservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsCmecServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsCmecServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sayant/projects/eventscmecservice/EventsCmecServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package sayant.projects.eventscmecservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsCmecServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
